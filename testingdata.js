77fff71b7c31d3a0239daf115bfe5f7a228acffa
Philip Xu <philipxjm1@gmail.com>
2016-01-11 00:05:21 -0500

    fixed testingdata.js

--- testingdata.js
+++ testingdata.js
@@ -1,41 +1,37 @@
-var profiles = {
-    [{
-        name: "Jessica Shi",
-        email: "jshi17@choate.edu",
-        class: "2017",
-        team_id: "roflcoptor",
-        school_id: "choate"
-    }, {
-        name: "Philip Xu",
-        email: "jxu16@choate.edu",
-        class: "2016",
-        team_id: "roflcoptor",
-        school_id: "choate"
-    }, {
-        name: "Prep Prepington",
-        email: "pprepington@exeter.edu",
-        class: "2017",
-        team_id: "lolcoptor",
-        school_id: "exeter"
-    }]
-}
+var profiles = [{
+    name: "Jessica Shi",
+    email: "jshi17@choate.edu",
+    class: "2017",
+    team_id: "roflcoptor",
+    school_id: "choate"
+}, {
+    name: "Philip Xu",
+    email: "jxu16@choate.edu",
+    class: "2016",
+    team_id: "roflcoptor",
+    school_id: "choate"
+}, {
+    name: "Prep Prepington",
+    email: "pprepington@exeter.edu",
+    class: "2017",
+    team_id: "lolcoptor",
+    school_id: "exeter"
+}];
 
-var teams = {
-    [{
-        name: "Rofl Coptor",
-        school_id: "choate",
-        level: "Varsity"
-    }, {
-        name: "Lol Coptor",
-        school_id: "exeter",
-        level: "Varsity"
-    }]
-}
 
-var schools = {
-    [{
-        name: "Choate Rosemary Hall"
-    }, {
-        name: "Philips Exeter Academy"
-    }]
-}
+var teams = [{
+    name: "Rofl Coptor",
+    school_id: "choate",
+    level: "Varsity"
+}, {
+    name: "Lol Coptor",
+    school_id: "exeter",
+    level: "Varsity"
+}];
+
+
+var schools = [{
+    name: "Choate Rosemary Hall"
+}, {
+    name: "Philips Exeter Academy"
+}];
