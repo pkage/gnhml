7caa55deafd5d0136c44355ad3b5cf9b0620cd8a
pkage <pkage@mit.edu>
2016-01-12 09:31:22 -0500

    added redirection after login

--- routing.js
+++ routing.js
@@ -1,19 +1,23 @@
 Router.route('/example', {
 	template: 'example'
-})
+});
 
 Router.route('/', {
 	template: "splash"
-}),
+});
 
 Router.route('/season', {
 	template: "season"
-}),
+});
 
 Router.route('/team', {
 	template: "team"
-})
+});
 
 Router.route('/admin', {
 	template: "admin"
-})
+});
+
+Router.route('/home', { 
+	template: 'home'
+});
