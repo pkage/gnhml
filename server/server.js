62d4de489f22f1d08896689f13f7013e1ff1d726
jshi17 <weiqian.shi@gmail.com>
2016-01-14 13:41:43 -0500

    finished structure of team/season tables

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ SeasonTable = new Meteor.Collection("season");
 TeamTable = new Meteor.Collection("team");
 
 Meteor.startup(function () {
+	var competition_dates = [];
 	if (Season.find().count() === 0) {
 		var teams = Teams.find({}).fetch();
 		var team_names = [];
@@ -15,20 +16,53 @@ Meteor.startup(function () {
 		}
 
 		var competitions = Competitions.find({}).fetch();
-		var competition_dates = [];
 		for (var i = 0; i < competitions.length; i++) {
-			competition_names.push(competitions[i].date);
+			competition_dates.push(competitions[i].date);
 		}
 
 		var scores = Scores.find({}).fetch();
 		var score_totals = []; // 2d array??
-		// query for all scores matching team and date + total
+		for (var i = 0; i < team_names.length; i++) {
+			for (var j = 0; j < competition_dates.length; j++) {
+				// query for all scores matching team and date + total
+				score_totals[i+j] = i+j; // temporary score
+			}
+		}
 		
 
 		for (var i = 0; i < team_names.length; i++) {
 			for (var j = 0; j < competition_dates.length; j++) {
 				SeasonTable.insert({
 					team: team_names[i],
+					date: competition_dates[j],
+					score: score_totals[i+j]
+				});
+			}
+		}
+	},
+
+	if (TeamTable.find().count() === 0) {
+		var profiles = Profiles.find({}).fetch();
+		var student_names = [];
+		for (var i = 0; i < profiles.length; i++) {
+			student_names.push(profiles[i].name);
+		}
+
+		// competitions same as previously, already added
+
+		var scores = Scores.find({}).fetch();
+		var score_totals = []; // 2d array??
+		for (var i = 0; i < student_names.length; i++) {
+			for (var j = 0; j < competition_dates.length; j++) {
+				// query for all scores matching student and date + total
+				score_totals[i+j] = i+j; // temporary score
+			}
+		}
+
+		for (var i = 0; i < student_names.length; i++) {
+			for (var j = 0; j < competition_dates.length; j++) {
+				TeamTable.insert({
+					student: student_names[i],
 					date: competition_dates[j],
 					score: score_totals[i+j]
 				});
