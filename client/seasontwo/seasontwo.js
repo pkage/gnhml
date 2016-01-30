1ebce7dde0a90adaa6ff096cb468c6e77899fa91
Philip Xu <philipxjm1@gmail.com>
2016-01-30 17:46:12 -0500

    cleaned some up, setting up for data strcuture clean up

--- client/seasontwo/seasontwo.js
+++ client/seasontwo/seasontwo.js
@@ -1,72 +1,76 @@
 Template.seasontwo.helpers({
-	// 'competition': function(){
-	// 	return Competitions.find({});
-	// },
+    // 'competition': function(){
+    // 	return Competitions.find({});
+    // },
 
-	// 'competition_date': function(){
-	// 	return this.date;
-	// },
+    // 'competition_date': function(){
+    // 	return this.date;
+    // },
 
-	// 'team': function(){
-	// 	return Teams.find({});
-	// },
+    // 'team': function(){
+    // 	return Teams.find({});
+    // },
 
-	// 'team_name': function(){
-	// 	return this.name;
-	// },
+    // 'team_name': function(){
+    // 	return this.name;
+    // },
 
-	// 'random_score': function(){
-	// 	return Math.floor(Math.random() * 10);
-	// }
+    // 'random_score': function(){
+    // 	return Math.floor(Math.random() * 10);
+    // }
 
-	'season_context': function() {
-		var tracking = [
-		{
-			field: 'name',
-			title: 'Teams'
-		}
-		];
+    'season_context': function() {
+        var tracking = [{
+            field: 'name',
+            title: 'Teams'
+        }];
 
-		var competitions = Competitions.find({}).fetch();
+        var competitions = Competitions.find({}).fetch();
 
-		var createFunc = function(comp_id) {
-			return function(value, ctx) {
-				console.log("competition_id: " + comp_id + "\nteam_id: " + ctx._id + "\nrelevant scores: " + Scores.find({competition_id: comp_id, team_id: ctx._id}).count());
-				
-				var competition_score = 0;
-				// map a function to each 
-				Scores.find({competition_id: comp_id, team_id: ctx._id}).map(function(doc) {
-					competition_score += doc.score;
-				})
-				console.log("\tscore:" + competition_score);
-				return competition_score;
-			}
-		}
+        var createFunc = function(comp_id) {
+            return function(value, ctx) {
+                console.log("competition_id: " + comp_id + "\nteam_id: " + ctx._id + "\nrelevant scores: " + Scores.find({
+                    competition_id: comp_id,
+                    team_id: ctx._id
+                }).count());
 
-		var randomName = function() {
-			var id_material = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-			var out = "";
-			for (var i = 0; i < 10; i++) {
-				out += id_material[Math.floor(Math.random() * id_material.length)];
-			}
-			return out;
-		}
+                var competition_score = 0;
+                // map a function to each
+                Scores.find({
+                    competition_id: comp_id,
+                    team_id: ctx._id
+                }).map(function(doc) {
+                    competition_score += doc.score;
+                })
+                console.log("\tscore:" + competition_score);
+                return competition_score;
+            }
+        }
 
-		for (var c = 0; c < competitions.length; c++) {
-			var _id = competitions[c]._id;
-			console.log('prepping: ' + _id);
-			tracking.push({
-				field: '',
-				title: Competitions.findOne(_id).date.toString(),
-				func: createFunc(_id)
-			})
-		}
+        var randomName = function() {
+            var id_material = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            var out = "";
+            for (var i = 0; i < 10; i++) {
+                out += id_material[Math.floor(Math.random() * id_material.length)];
+            }
+            return out;
+        }
 
-		return {
-			db: Teams,
-			selector: {},
-			tracking: tracking,
-			hoverable: true
-		}
-	}
+        for (var c = 0; c < competitions.length; c++) {
+            var _id = competitions[c]._id;
+            console.log('prepping: ' + _id);
+            tracking.push({
+                field: '',
+                title: Competitions.findOne(_id).date.toString(),
+                func: createFunc(_id)
+            })
+        }
+
+        return {
+            db: Teams,
+            selector: {},
+            tracking: tracking,
+            hoverable: true
+        }
+    }
 })
