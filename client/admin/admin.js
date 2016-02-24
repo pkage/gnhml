3155bb0cefa4052253d6a55fd9b4b119070cfe3d
jshi17 <weiqian.shi@gmail.com>
2016-02-24 11:45:25 -0500

    now it adds schools but only kind of cause you have to do it from the server side but i need to push

--- client/admin/admin.js
+++ client/admin/admin.js
@@ -67,6 +67,18 @@ Template.admin_schools.events({
 	'submit #updateSchoolName': function(ev) {
 		ev.preventDefault();
 		Meteor.call('updateSchoolName', Session.get('selected'), $('[name="school_name"]').val())
+	},
+	'click #add-button': function() {
+		console.log("HELLO HELLO HELLO HELLO");
+		$('#add-school-form-modal').transition('fade down');
+	},
+
+	'submit #add-school-form': function(ev) {
+		ev.preventDefault();
+		Schools.insert({
+			name: ev.target.schoolname.text
+		});
+		$('#add-school-form-modal').transition('fade down');
 	}
 })
 
