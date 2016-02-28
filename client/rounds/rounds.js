ddaab043ef158c72feb6ac1d0da38a5a3fd92322
jshi17 <weiqian.shi@gmail.com>
2016-02-27 23:03:35 -0500

    in process of trying to do stuff

--- client/rounds/rounds.js
+++ client/rounds/rounds.js
@@ -1,5 +1,13 @@
+Template.rounds.onRendered({
+	// Session.set('selectedRounds', []);
+})
+
 Template.rounds.events({
 	'click .toggle': function(ev) {
+		var col = $(ev.target).parent().children().index($(this));
+  		var row = $(ev.target).parent().parent().children().index($(this).parent());
+		console.log(col + " " + row)
+
 		if ($(ev.target).hasClass('toggle-inactive')){
 			ev.target.classList.remove('toggle-inactive');
 			ev.target.classList.add('toggle-active');
--- client/studentselect/studentselect.js
+++ client/studentselect/studentselect.js
@@ -1,13 +1,46 @@
 Template.studentselect.onRendered(function() {
-    $('.ui.dropdown').dropdown();
+    /*$('.ui.dropdown').dropdown({
+    	'action': 'hide',
+    	'onChange': function() {
+    		// create an array of all matching selects
+			var selects = $('[data-type="student"]').toArray();
+
+			// turn those objects into selects into value
+			selects = _.map(selects, function(el) {
+				return $(el).val()
+			});
+
+
+			Session.set('disabledStudents', selects);
+		}
+	});*/
+Session.set('disabledStudents', []);
 });
 
 Template.studentselect.helpers({
 	'currentProfile': function() {
-        return Profiles.findOne({account_id: Meteor.userId()});
-        console.log(Profiles.findOne({account_id: Meteor.userId()}));
-    },
-    'student': function(){
-    	return Profiles.find({team_id: this.team_id});
-    }
+		return Profiles.findOne({account_id: Meteor.userId()});
+	},
+	'student': function(){
+		return Profiles.find({team_id: this.team_id});
+	},
+	'disabledStudent': function() {
+		return _.contains(Session.get('disabledStudents'), this._id) ? 'disabled' : '';
+	}
+})
+
+Template.studentselect.events({
+	'change [data-type="student"]': function() {
+		console.log('changed');
+		// create an array of all matching selects
+		var selects = $('[data-type="student"]').toArray();
+
+		// turn those objects into selects into value
+		selects = _.map(selects, function(el) {
+			return $(el).val()
+		});
+
+
+		Session.set('disabledStudents', selects);
+	}
 })
