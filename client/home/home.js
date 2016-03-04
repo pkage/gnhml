615ba92696d35c73703f2fbcd145078d2a22a43f
Philip Xu <philipxjm1@gmail.com>
2016-03-04 09:14:34 -0500

    made twitter feed scrollable, added new list of highscores for student highscores

--- client/home/home.js
+++ client/home/home.js
@@ -6,6 +6,22 @@ Template.home.helpers({
             sortable: false,
             tracking: [{
                 field: 'date',
+                title: 'Upcoming Competition Dates',
+                func: function(value, ctx) {
+                    return String(value).substring(0, 15);
+                }
+            }],
+            hoverable: true,
+            customCSS: "table-style-class"
+        }
+    },
+    'highscore_context': function() {
+        return {
+            db: Competitions,
+            selector: {},
+            sortable: false,
+            tracking: [{
+                field: 'date',
                 title: 'Competition Date',
                 func: function(value, ctx) {
                     return String(value).substring(0, 15);
